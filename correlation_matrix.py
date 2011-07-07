394b092113f2d04b7af042033e4127756b1d7b21
Peter Combs <pcombs@gmail.com>
2011-07-06 17:57:04 -0700

    Added filter_strains

--- correlation_matrix.py
+++ correlation_matrix.py
@@ -1,13 +1,49 @@
 import numpy as np
 from sequence_tools import read_fasta
+from collections import Counter
 
-gag_seq_file = '../data/HIV1_CON_2004_GAG_PRO.fasta'
+def filter_strains(data):
+    """ Filters out strains that are sufficiently different from the rest
+    
+    On my first look-through of this data, some of the aligned proteins are
+    different lengths.  Since we don't know whether they were clipped at the
+    beginning, the end, or what, we'll just take strains that are the most
+    common length.
+
+    In principle, you could have other filter conditions in here too!
+    """
+    # This is a python data type that counts things, and can tell you the most
+    # common element
+    length_counter = Counter()
+
+    # Count up the lengths of the strains
+    for strain in data:
+        length_counter[len(strain)] += 1
+
+    # A Counter's most_common method returns a list of tuples, with the first
+    # element as the item, and the second element as the count of that item,
+    # with the list sorted in descending order by counts
+    most_common = length_counter.most_common()[0][0]
+
+    lengths = [len(strain) for strain in data]
+    # We need a copy of the data so we don't modify the list while looping over
+    # it, which leads to odd behavior.
+    good_data = []
+
+    for sequence in data_copy:
+        if len(sequence) == most_common:
+            good_data.append(sequence)
+
+    # Must not forget the return statement
+    return good_data
+
 
 gag_data_full = read_fasta(gag_seq_file)
 
 gag_data = [gag_data[name] for name in gag_data_full]
 
-filter_strains(gag_data)
+# Remove strains that aren't similar enough to each other
+gag_data = filter_strains(gag_data)
 
 consensus_sequence = get_consensus(gag_data)
 
